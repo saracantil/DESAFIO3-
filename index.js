827b51798f5940c60873a5bfa2f9a22b300af819
SaraCantil <>
2021-05-28 00:37:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var cliente = prompt("Qual o valor da sua compra?");
+
+var aplicativo = prompt("você usou o aplicativo?");
+
+if (aplicativo == 'sim') {
+  console.log("voce ira receber um desconto de 5%");
+  console.log(" o valor final sera, " + 5 / 100 * cliente); 
+  
+}
+else {
+  console.log("ok, iremos finalizar sua compra"); 
+} 
+
+// FAÇA UM PROGRAMA QUE CAUCULE UM DESCONTO DE 5% DE QUALQUER VALOR//
+
+
+
+
